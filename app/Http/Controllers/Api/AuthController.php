c093d4bb9e098b9370774c1d5f07e7f693c7414e
Giuseppe Spataro <spatyx@gmail.com>
2025-04-06 16:23:25 +0200

    feat(Auth): implement user registration

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * User registration
+     */
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        $user = User::create($validated);
+
+        return response()->json($user, 201);
+    }
+}
