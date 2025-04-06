132ce165790a0e7a09196200a82bc4361d402266
Giuseppe Spataro <spatyx@gmail.com>
2025-04-06 16:23:37 +0200

    test(Auth): test registration

---
+++ tests/Feature/AuthApiTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+describe('auth:register', function () {
+    it('creates new user', function () {
+        $password = fake()->password(8);
+        $data = [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        $response = $this->postJson('/api/register', $data);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('users', [
+            'email' => $data['email']
+        ]);
+    });
+
+    it('prevents same email', function () {
+        $user = User::factory()->create();
+        $password = fake()->password(8);
+        $data = [
+            'name' => fake()->name(),
+            'email' => $user->email,
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        $response = $this->postJson('/api/register', $data);
+
+        $response->assertStatus(422);
+    });
+});
