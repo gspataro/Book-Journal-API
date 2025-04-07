e574a5b99c579ebee742cf6434d300c81743efd0
Giuseppe Spataro <spatyx@gmail.com>
2025-04-07 10:18:33 +0200

    new(migration): create books table

---
+++ database/migrations/2025_04_07_081320_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn');
+            $table->string('title');
+            $table->text('authors');
+            $table->text('publisher')->nullable();
+            $table->text('edition')->nullable();
+            $table->string('language')->nullable();
+            $table->datetime('publication_date')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('pages')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
